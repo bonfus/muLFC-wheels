f5ec16755781723fe442819ca729c265106c3ffc
Pietro Bonfa <bonfus@gmail.com>
2017-09-13 20:36:42 +0200

    Added missing config.sh

---
+++ config.sh
@@ -0,0 +1,11 @@
+function pip_opts {
+    :
+}
+function pre_build {
+    # Any stuff that you need to do before you start building the wheels
+    # Runs in the root directory of this repository.
+    :
+}
+function run_tests {
+    python --version
+}
